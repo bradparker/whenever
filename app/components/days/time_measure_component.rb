61f5420df2ed1ae8d322a53ea90b193e945a410f
Brad Parker <hi@bradparker.com>
2020-04-04 10:54:20 +1000

    Spike at week and day grid lines

--- app/components/days/time_measure_component.rb
+++ app/components/days/time_measure_component.rb
@@ -1,5 +1,10 @@
 class Days::TimeMeasureComponent < ViewComponent::Base
-  def initialize
+  def initialize(display_numbers: true)
+    @display_numbers = display_numbers
+  end
+
+  def display_numbers?
+    @display_numbers
   end
 
   def beginning_of_day
